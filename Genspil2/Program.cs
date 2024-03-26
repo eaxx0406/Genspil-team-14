dde417f334112c288ab24a7f6c0f0beab98ee40c
Ea Overvad <ea@0vervad.dk>
2024-03-26 17:07:41 +0100

    Oprettet solutin

---
+++ Genspil2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Genspil14.Gui;
+using Genspil14.Model.Filehandlers;
+using System.ComponentModel.Design;
+
+namespace Genspil14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           //Hej Team 14
+        }
+    }
+}
