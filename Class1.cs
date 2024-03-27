552482488aaa0c16b9b0506fff9b5a640aa49067
Per Bauer <peba63298@edu.ucl.dk>
2024-03-27 09:29:57 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Class1
+    {
+
+        
+        public override string ToString() => $"{{gamenamer: {Name}, maxnumberplayers: {MaxNumberPlayers}, minnumberplayers: {MinNumberPlayers}, agemax: {MaxAgeGroup}, agemin: {MinAgeGroup}, price: {Price}, id: {ID}, genre: {Genre}, state: {State}}}";
+
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string State { get; set; }
+        public int ID { get; set; }
+        public double Price { get; set; }
+        public int MaxNumberPlayers { get; set; }
+        public int MinNumberPlayers { get; set; }
+        public int MaxAgeGroup { get; set; }
+        public int MinAgeGroup { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Class1 { Name = "Per Bauer", MaxNumberPlayers = 6, MinNumberPlayers = 2, MaxAgeGroup = 10, MinAgeGroup = 5, ID = 2, Price = 50, Genre = "Familie game", State = "Perfect"});
+
+            }
+
+    }
+
+}
+
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
